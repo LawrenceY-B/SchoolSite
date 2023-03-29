3ffed892bd36b1a9dda532ac462b1d4bba86692e
Lawrence Yirenkyi-Boafo <lawrencekyb@gmail.com>
2023-03-29 17:18:07 +0000

    changed font

--- src/app/modules/components/contact/contact.component.ts
+++ src/app/modules/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ISchoolData } from 'src/app/shared/model/result.model';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+message: string = '';
+email: string = '';
+name: string='';
+formData!: ISchoolData;
 
-  constructor() { }
+  constructor(    private apiService: ApiService,
+    ) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit() {
+    this.formData= {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+  this.formData
+    this.apiService.postmessage(this.formData)
+      console.log(this.formData)
+  
+  }
 }
